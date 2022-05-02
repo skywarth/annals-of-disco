eb1c2010180dd5d96ad5399c9ac4b6ead5331a5e
skywarth <yigitk.ersoy@gmail.com>
2022-05-02 20:56:06 +0300

    Create laravel.php

---
+++ information-technology/software/code/php/laravel.php
@@ -0,0 +1,12 @@
+### Cool packages
+
+#### Health/Status
+- https://github.com/antonioribeiro/health
+- https://github.com/spatie/laravel-health
+
+#### Error
+- Flare
+
+
+#### CI/CD
+- https://www.laravel-enlightn.com/ (Performance and Security Insight)
